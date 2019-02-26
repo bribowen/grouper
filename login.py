e669f20226a04cc850bbc28f8bd93041aa79c886
Brian Bowen <babowentx@gmail.com>
2019-02-26 11:56:06 -0600

    Added logout method

--- login.py
+++ login.py
@@ -21,6 +21,11 @@ def do_admin_login():
         flash('wrong password!')
     return home()
 
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return home()
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
