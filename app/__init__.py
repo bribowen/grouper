fcb68682b5d483caf055a1e449e111bbe25b6aca
Brian Bowen <babowentx@gmail.com>
2019-03-01 16:14:21 -0600

    Restructuring

--- app/__init__.py
+++ app/__init__.py
@@ -86,6 +86,6 @@ def show_post(post_id):
     #show the post with the given id
     return 'Post %d' % post_id
 
-def __name__ == "__main__":
+if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
