f0a6b2d1f57fe5903d57badd6b607e2f37c966bd
Brian Bowen <babowentx@gmail.com>
2019-03-05 17:47:24 -0600

    Figuring out Jinja

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
-from app import db
-
+"""
 #Dim_Project
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -49,3 +48,46 @@ class UserSkills(db.Model)
 class UserInterest(db.Model):
 	id = db.Column(db.Integer, db.ForeignKey('interest.id'), primary_key=True)
 	uin = db.Column(db.Integer, db.ForeignKey('user.uin'), primary_key=True)
+"""
+
+#Class to use for individual projects. Takes in the poster's first/last name, the project name,
+#the project type (individual or for a class), and a brief description.
+class Project:
+    def __init__(self, post_id, posterfname, posterlname, projname, projtype, description):
+        self.post_id = post_id
+        self.posterfname = posterfname
+        self.posterlname = posterlname
+        self.projname = projname
+        self.type = projtype
+        self.description = description
+    #Method for updating parts of the project that can be changed.
+    def Update(name, ptype, description):
+        self.name = name
+        self.type = ptype
+        self.description = description
+
+#Class to use for individual profiles. Takes the user's id (UIN), first name, last name, persona (student or professor),
+#email, primary contact, a list of associated interests, and a list of associated projects
+class Profile:
+    def __init__(self, uin, fname, lname, persona, email, prcontact, interests, projects):
+        self.id = uin
+        self.fname = fname
+        self.lname = lname
+        self.persona = persona
+        self.email = email
+        self.prcontact = prcontact
+        self.interests = interests
+        self.projects = projects
+
+    def Update(fname, lname, persona, email, prcontact, interests):
+        self.fname = fname
+        self.lname = lname
+        self.persona = persona
+        self.email = email
+        self.prcontact = prcontact
+        self.interests = interests
+
+    #Method to create a project associated with a profile. Allows filling in required info (like poster's name)
+    #through the Profile's stored info.
+    def CreateProject(projname, projtype, description):
+        newProject = Project(self.fname, self.lname, projname, projtype, description)
