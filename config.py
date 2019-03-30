769409a9920e6fb720663ec3291ad5d41d87ae46
Brian Bowen <babowentx@gmail.com>
2019-03-30 13:22:56 -0500

    Changed permissions

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://Brian:Fenthorn!1@grouper.czauevigug7j.us-east-2.rds.amazonaws.com/grouper'
+    SQLALCHEMY_DATABASE_URI = 'mysql://Grouper:Grouper!1@grouper.czauevigug7j.us-east-2.rds.amazonaws.com/grouper'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 5
